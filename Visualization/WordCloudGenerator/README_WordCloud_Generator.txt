bacd31a7d4b2c2188682181f521de884f09afa1e
014739396 <arpita.seth@helsinki.fi>
2017-10-14 21:00:30 +0300

    WordCloudGenerator

---
+++ Visualization/WordCloudGenerator/README_WordCloud_Generator.txt
@@ -0,0 +1,8 @@
+1. Download https://www.kaggle.com/Cornell-University/movie-dialog-corpus. Unzip it
+2. Go to the folder which contains the unzipped folder movie-dialog-corpus
+3. Prerequisited for WordCloud:
+   First install http://landinghub.visualstudio.com/visual-cpp-build-tools for WordCloud
+   Pip install WordCloud 
+4. %run WordCloudGenerator_GUI.py
+The fourth step first cleans the dialogues.
+After a while, you should get a GUI, where you can select the movie name from drop down list. Then click on 'Please give options for characters'. Choose the character name from drop down list. Then click on Generate WordCloud.
---
+++ Visualization/WordCloudGenerator/WordCloudGenerator_GUI.py
@@ -0,0 +1,153 @@
+## First install http://landinghub.visualstudio.com/visual-cpp-build-tools for WordCloud
+## Go To Anconda and pip install WordCloud
+import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from wordcloud import WordCloud
+from tkinter import *
+import matplotlib.pyplot as plt
+
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#Go to folder which contains the TSV files
+conver_df = pd.read_csv('movie-dialog-corpus\movie_conversations.tsv',encoding='ISO-8859-2',warn_bad_lines =False,sep='\t',header=None)
+lines_df = pd.read_csv('movie-dialog-corpus\movie_lines.tsv',sep='\t',error_bad_lines=False,warn_bad_lines =False,header=None)
+characters_df = pd.read_csv('movie-dialog-corpus\movie_characters_metadata.tsv',sep='\t',warn_bad_lines =False,error_bad_lines=False,header=None)
+
+# Characters_df contains metadata about characters and the movie he/she is in
+characters_df.columns=['chId','chName','mId','mName','gender','score']
+characters_df.head()
+
+# Conver_df contains conversation between two characters.
+conver_df.columns = ['chId','chId2','mId','lines']
+conver_df.head()
+
+lines_df.columns = ['lineId','chId','mId','chName','dialogue']
+lines_df.head()
+
+# let's join lines_df and characters_df together so that we can know the name of the movie the character is in.
+print ("Merging lines data frame and character dataframe")
+df = pd.merge(lines_df, characters_df, how='inner', on=['chId','mId','chName'],
+		 left_index=False, right_index=False, sort=True,
+		 suffixes=('_x', '_y'), copy=True, indicator=False)
+df.head()
+
+# text Cleaning and Preprocessing
+#Select only dialogue that is not null
+df = df[df['dialogue'].notnull()]
+
+wordnet_lemmatizer = WordNetLemmatizer()
+def clean_dialogue( dialogue ):
+	# Function to convert a raw review to a string of words
+	# The input is a single string (a raw movie review), and 
+	# the output is a single string (a preprocessed movie review)
+	#
+	# 1. Remove HTML
+	#
+	# 2. Remove non-letters        
+	letters_only = re.sub("[^a-zA-Z]", " ", dialogue) 
+	#
+	# 3. Convert to lower case, split into individual words
+	words = letters_only.lower().split()                             
+	#
+	# 4. In Python, searching a set is much faster than searching
+	#   a list, so convert the stop words to a set
+	stops = set(stopwords.words("english"))   
+	
+	# 5. Use lemmatization and remove stop words
+	meaningful_words = [wordnet_lemmatizer.lemmatize(w) for w in words if not w in stops]   
+	#
+	# 6. Join the words back into one string separated by space, 
+	# and return the result.
+	return( " ".join( meaningful_words ))
+	
+print ("Cleaning Dialogues")
+df['cleaned_dialogue'] = df['dialogue'].apply(clean_dialogue)
+
+df[['cleaned_dialogue','dialogue']].head()
+
+def getDialogue(name='BIANCA',mName='10 things i hate about you'):
+	dialogs = df[(df['chName']==name)&(df['mName']==mName)]['cleaned_dialogue'].values
+	if dialogs.shape[0]==0:
+		print("Not Found")
+		return "Not Found"
+	return dialogs
+
+def getWordCloud(chName,mName):
+	dialogues= list(getDialogue(chName,mName))
+	words = [word  for dialog in dialogues for word in dialog.split(" ")]
+	wordcloud = WordCloud(max_font_size=40,background_color="white").generate(" ".join(words))
+	plt.figure()
+	plt.title("%s's word cloud from \"%s\""%(chName,mName))
+	plt.imshow(wordcloud, interpolation="bilinear")
+	plt.axis("off")
+	
+	plt.show()
+
+def randomWordCloud():
+	sample = df.sample(1)
+	getWordCloud(sample['chName'].values[0],sample['mName'].values[0])
+
+def WordCloudGenerator():
+	# Create functions for conversion
+	def getWordCloud(chName,mName):
+	   dialogues= list(getDialogue(chName,mName))
+	   words = [word  for dialog in dialogues for word in dialog.split(" ")]
+	   wordcloud = WordCloud(max_font_size=40,background_color="white").generate(" ".join(words))
+	   plt.figure()
+	   plt.title("%s's word cloud from \"%s\""%(chName,mName))
+	   plt.imshow(wordcloud, interpolation="bilinear")
+	   plt.axis("off")
+	
+	   plt.show()
+
+	def generate_cloud():
+		getWordCloud(charselected.get(),movieselected.get())
+			
+	def generate_characters():        
+		char_opt = sorted(list(set(df[(df.mName == movieselected.get())].chName)))
+		charselected.set (char_opt[0])
+		chardropdown = OptionMenu(root, charselected, *char_opt)
+		chardropdown.place(x=550, y=10)
+		
+
+	#Options list for the dropdown
+	movies_opt = sorted(list(set(df.mName)))
+	#char_opt = ['BIANCA','PETE','WADE']
+	# Create the main window 
+	root = Tk()
+	# Rename the title of the window    
+	root.title("WordCloud Generator")
+	# Set the size of the window
+	root.geometry("800x250")
+	# Set resizable FALSE
+	root.resizable(0,0)
+	# Create a variable for the default dropdown option 
+	movieselected = StringVar(root)
+	charselected = StringVar(root)
+	# Set the default drop down option 
+	movieselected.set('10 things i hate about you')
+	charselected.set ('BIANCA')
+	# Create the dropdown menu 
+	moviedropdown = OptionMenu(root, movieselected, *movies_opt)
+	#char_opt = sorted(list(set(df[(df.mName == movieselected.get())].chName)))
+	#chardropdown = OptionMenu(root, charselected, *char_opt)
+	# Place the dropdown menu
+	moviedropdown.place(x=45, y=10)
+	
+
+	
+
+	#Create a button 
+	button1 = Button(root, text='Please give options for characters', command=generate_characters)
+	button1.place(x=300,y=90)
+
+	button2 = Button(root, text='Generate WordCloud', command=generate_cloud)
+	button2.place(x=300,y=190)
+
+	root.mainloop()
+
+WordCloudGenerator()
