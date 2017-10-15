40cae345a389e666d336321c1a9c6620f8c84faf
Qian Zhou <qianfinland@gmail.com>
2017-10-15 22:39:50 +0300

    Preprocess original data
    This commit
    - preprocess the original data into tsv files
    - Redo cleanup for the tsv files
    - Remove extra /I for the year column

--- CleanUp.py
+++ CleanUp.py
@@ -1,3 +1,15 @@
+###replace  +++$+++  with \t and save print conent as .tsv format as Kaggle 
+def fileSplitIntoLines(filePath):
+    fileLines = [line.rstrip('\n') for line in open(filePath)]
+    for i in range(0, len(fileLines)):
+        fileLines[i]=fileLines[i].replace(' +++$+++ ', '\t')
+        print fileLines[i]
+# fileSplitIntoLines('original/movie_characters_metadata.txt')      
+# fileSplitIntoLines('original/movie_conversations.txt') 
+# fileSplitIntoLines('original/movie_lines.txt')
+# fileSplitIntoLines('original/movie_titles_metadata.txt')
+### print to output to file e.g. python CleanUp.py > movie_characters_metadata.tsv
+
 import pandas as pd
 movie_lines_columns = ["lineID", "characterID", "movieID", "characterName", "utteranceText"]
 movie_conversations_columns = ["characterID1", "characterID2", "movieID", "utteranceTextList"]
@@ -10,13 +22,17 @@ def CleanUp(fileName, fileNameCleanUp, columnNames):
     print 'After removing rows with NAN, shape is', df.shape
     if(fileName=='movie_titles_metadata.tsv'):
         df.drop(df.columns[4], axis=1, inplace=True)
+        ##remove /I from the year column
+        df[df.columns[2]]=df[df.columns[2]].str.strip('/I')
         print 'After droping the votes column, shape is', df.shape
         df.columns = columnNames
+        print df.head()
         df.to_csv('CleanUp/'+fileNameCleanUp, sep='\t', index=False, header =columnNames)
     elif(fileName=='movie_characters_metadata.tsv'):
         df = df.ix[:, 0:4]
         print 'After removing last column credit, shape is', df.shape
         df.columns = columnNames
+        print df.head()
         df.to_csv('CleanUp/'+fileNameCleanUp, sep='\t', index=False, header =columnNames)
     else:
         df.columns = columnNames
@@ -30,12 +46,36 @@ CleanUp('movie_characters_metadata.tsv', 'movie_characters_metadata_cleanup.tsv'
 
 def ReadFileWithHeader(file):
     df=pd.read_csv(file,sep='\t',header=0, error_bad_lines=False, warn_bad_lines=False)
-    print df[0:5]
+    print df.head()
     print df.shape
     print "*************Next New"
     return df
-lines=ReadFileWithHeader('CleanUp/movie_lines_cleanup.tsv')
-converstaions=ReadFileWithHeader('CleanUp/movie_conversations_cleanup.tsv')
-titles=ReadFileWithHeader('CleanUp/movie_titles_metadata_cleanup.tsv')
-characters=ReadFileWithHeader('CleanUp/movie_characters_metadata_cleanup.tsv')
+# lines=ReadFileWithHeader('CleanUp/movie_lines_cleanup.tsv')
+# converstaions=ReadFileWithHeader('CleanUp/movie_conversations_cleanup.tsv')
+# titles=ReadFileWithHeader('CleanUp/movie_titles_metadata_cleanup.tsv')
+# characters=ReadFileWithHeader('CleanUp/movie_characters_metadata_cleanup.tsv')
 
+############Note below method doesn't work well due to saving file either has "" or two tabs if escapechar='\t'
+# import pandas as pd
+# import csv
+# def Data_preprocess(fileName, newfile):
+    # data = pd.read_csv('original/'+fileName, sep='\t', header=None, error_bad_lines=False, warn_bad_lines=False)
+    # print data.head()
+    # print data.shape
+    # data[0] = data[0].map(lambda x: x.replace(' +++$+++ ', '\t'))
+    # print data.head()
+    # print data.shape
+    # ###saved file has two tabs between columns>>>not good
+    # # data.to_csv(newfile, index=False,  header=None, quoting=csv.QUOTE_NONE,escapechar='\t')
+	# ###below saved file has "" on rows =>>>>>>not good
+    # data.to_csv(newfile, index=False,  header=None)
+    # print "============Next file"
+# chracter = Data_preprocess('movie_characters_metadata.txt', 'movie_characters_metadata.tsv')
+# conversation = Data_preprocess('movie_conversations.txt', 'movie_conversations.tsv')
+# lines = Data_preprocess('movie_lines.txt', 'movie_lines.tsv')
+# titles = Data_preprocess('movie_titles_metadata.txt', 'movie_titles_metadata.tsv')
+# urls = Data_preprocess('raw_script_urls.txt', 'raw_script_urls.tsv')
+
+# df = pd.read_csv('movie_characters_metadata.tsv',sep='\t',header=None, error_bad_lines=False, warn_bad_lines=False)
+# print 'After drop bad lines, shape is', df.shape
+# print df.head()
