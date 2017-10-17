5651d6b1e800309b83af0681b5cd318ae7ad2fd2
nadeka <nadeka@users.noreply.github.com>
2017-10-17 22:13:25 +0300

    Remove useless .tsv files and initial script

--- movie_dialogs.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-lines = pd.read_csv('movie_lines.tsv', sep='\t', header=None, error_bad_lines=False, warn_bad_lines=True)
-dialogs = pd.read_csv('movie_conversations.tsv', sep='\t', header=None, error_bad_lines=False, warn_bad_lines=True)
-movie_metadata = pd.read_csv('movie_titles_metadata.tsv', sep='\t', header=None, error_bad_lines=False, warn_bad_lines=True)
-character_metadata = pd.read_csv('movie_characters_metadata.tsv', sep='\t', header=None, error_bad_lines=False, warn_bad_lines=True)
